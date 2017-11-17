7a87e8dd3c73a665b35232f05db5d95c44d0518f
wuhoudefeng <2662643959@qq.com>
2017-11-17 14:16:17 +0800

    first

---
+++ aa.py
@@ -0,0 +1,15 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============== RESTART: D:\4444\Git\python\python\python\a1.py ==============
+[Errno 10060] 
+>>> 
+============== RESTART: D:\4444\Git\python\python\python\a2.py ==============
+>>> 
+>>> 
+============== RESTART: D:\4444\Git\python\python\python\a3.py ==============
+OK
+>>> 
+============== RESTART: D:\4444\Git\python\python\python\a4.py ==============
+OK
+>>> 
